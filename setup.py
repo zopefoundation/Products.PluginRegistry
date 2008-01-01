eeb2b0ebdcf939e6a944203e63f8bcc7e131ba72
Wichert Akkerman <wichert@wiggy.net>
2008-01-01 14:43:18 +0000

    We need setuptools

--- setup.py
+++ setup.py
@@ -48,7 +48,10 @@ setup(name='Products.PluginRegistry',
       include_package_data=True,
       namespace_packages=['Products'],
       zip_safe=False,
-      #install_requires=['Zope >= 2.10']
+      install_requires=[
+          'setuptools',
+#          'Zope >= 2.10'
+          ],
       entry_points="""
       [zope2.initialize]
       Products.PluginRegistry = Products.PluginRegistry:initialize
