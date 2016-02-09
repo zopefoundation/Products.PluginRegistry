5e5afa896ff6f73c193a6dea63c1f5de029b3617
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:33:18 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 package = os.path.join(here, 'Products', 'PluginRegistry')
 
+
 def _package_doc(name):
     f = open(os.path.join(package, name))
     return f.read()
@@ -19,38 +20,42 @@ with open('README.rst') as f:
 with open('CHANGES.rst') as f:
     CHANGES = f.read()
 
-setup(name='Products.PluginRegistry',
-      version=VERSION,
-      description='Configure application plugins based on interfaces',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name='Products.PluginRegistry',
+    version=VERSION,
+    description='Configure application plugins based on interfaces',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Framework :: Zope2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Zope Public License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development",
         "Topic :: System :: Archiving :: Packaging",
-        "Topic :: Software Development :: Libraries :: "
-                "Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    keywords='web application server zope zope2',
+    author="Zope Foundation and Contributors",
+    author_email="zope-cmf@lists.zope.org",
+    url="https://pypi.python.org/pypi/Products.PluginRegistry",
+    license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['Products'],
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Zope2 >= 2.12',
+        'Products.GenericSetup'
         ],
-      keywords='web application server zope zope2',
-      author="Zope Foundation and Contributors",
-      author_email="zope-cmf@lists.zope.org",
-      url="http://pypi.python.org/pypi/Products.PluginRegistry",
-      license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
-      packages=find_packages(),
-      include_package_data=True,
-      namespace_packages=['Products'],
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Zope2 >= 2.12',
-          'Products.GenericSetup'
-          ],
-      entry_points="""
-      [zope2.initialize]
-      Products.PluginRegistry = Products.PluginRegistry:initialize
-      """,
-      )
+    entry_points="""
+    [zope2.initialize]
+    Products.PluginRegistry = Products.PluginRegistry:initialize
+    """,
+    )
