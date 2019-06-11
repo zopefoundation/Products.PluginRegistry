5c13c0f5b488ca83f2e042a99258141a1ce095cd
Godefroid Chapelle <gotcha@bubblenet.be>
2019-06-11 14:09:20 +0200

    extend IPluginRegistry with movePluginsTop

--- Products/PluginRegistry/PluginRegistry.py
+++ Products/PluginRegistry/PluginRegistry.py
@@ -166,6 +166,18 @@ class PluginRegistry(SimpleItem):
         self._plugins[plugin_type] = tuple(plugins)
 
     @security.protected(ManageUsers)
+    def movePluginsTop(self, plugin_type, ids_to_move):
+
+        """ See IPluginRegistry.
+        """
+        ids = list(self._getPlugins(plugin_type))
+        indexes = list(map(ids.index, ids_to_move))
+        indexes.sort()
+        for i1 in indexes:
+            ids.insert(0, ids.pop(i1))
+        self._plugins[plugin_type] = tuple(ids)
+
+    @security.protected(ManageUsers)
     def movePluginsUp(self, plugin_type, ids_to_move):
 
         """ See IPluginRegistry.
