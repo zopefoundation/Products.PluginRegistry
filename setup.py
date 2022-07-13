7b8245ee0e871f1c2b0726195043d6f291fcf6d7
Michael Howitz <mh@gocept.com>
2022-07-13 08:10:58 +0200

    Preparing release 1.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('CHANGES.rst') as f:
 
 setup(
     name='Products.PluginRegistry',
-    version='1.10.dev0',
+    version='1.10',
     description='Configure application plugins based on interfaces',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
